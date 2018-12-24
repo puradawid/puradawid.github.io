2f128fa67a821b82f95b4bf3d0787206793b4e0c
Dawid Pura <puradawid@gmail.com>
2018-12-24 17:55:00 +0100

    Remove popup styles and scripts that are unused

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,5 +1,36 @@
+// GTAG events
 $('.site-nav a.menu-icon').on('click', function () {
   if (gtag) {
     gtag('event', 'opened_mobile_menu', { event_category: "navigation" });
   }
+});
+
+(function ($) {
+  var hasPopuped = false;
+
+  $(window).on('scroll', function(event) {
+    var scrollValue = $(window).scrollTop();
+    if (scrollValue >= 500) {
+      if (!hasPopuped) {
+        hasPopuped = true;
+        if (gtag) {
+          gtag('event', 'scrolled_down', { event_category: "reading" });
+        }
+      }
+    }
+  });
+}(jQuery));
+
+// REGISTERING NEWSLETTER
+$("form.newsletter").on("submit", function() {
+  if (gtag) gtag('event', 'subscribed_newsletter', { event_category: "newsletter" });
+  $.ajax({
+    url: "/signup",
+    method: "POST",
+    dataType: "text",
+    headers: { "Accept": "*/*" },
+    data: { email: $('form.newsletter input[type=email]').val() }
+  });
+  $('form').toggleClass('done');
+  return false;
 });
