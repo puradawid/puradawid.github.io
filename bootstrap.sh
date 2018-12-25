0c1fe2d00ec433b491e41b26b50d3fdcf7221846
Dawid Pura <puradawid@gmail.com>
2018-12-25 11:25:35 +0100

    Add future posts for Jekyll build

--- bootstrap.sh
+++ bootstrap.sh
@@ -3,4 +3,4 @@ apt-get install -y npm nodejs ruby gem ruby-dev build-essential
 gem install bundler jekyll
 npm install -g http-server
 
-cd /vagrant && screen -dmS jekyll jekyll build --config _config.yml,_config_dev.yml --force_polling --watch && screen -dmS http-server http-server -p 80 _site
+cd /vagrant && screen -dmS jekyll jekyll build --config _config.yml,_config_dev.yml --future --force_polling --watch && screen -dmS http-server http-server -p 80 _site
