5950368c215d147a5efd19d077dfda673140b0f1
Dawid Pura <puradawid@gmail.com>
2019-01-21 19:01:08 +0100

    Add witcher blog post (#9)

--- bootstrap.sh
+++ bootstrap.sh
@@ -1,6 +1,6 @@
 apt-get update
 apt-get install -y npm nodejs ruby gem ruby-dev build-essential
-gem install bundler jekyll
+gem install bundler jekyll jekyll-sitemap
 npm install -g http-server
 
 cd /vagrant && screen -dmS jekyll jekyll build --config _config.yml,_config_dev.yml --future --force_polling --watch && screen -dmS http-server http-server -p 80 _site
